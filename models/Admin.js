9623c258e5552c8bda812c477fe920ef1ba1f501
AgMac <angelia.gyq@gmail.com>
2018-02-05 09:55:33 -0500

    added user in admin

--- models/Admin.js
+++ models/Admin.js
@@ -1,8 +1,13 @@
 var mongoose = require('mongoose')
 
 var AdminSchema = new mongoose.Schema({
+  user: {
+  	type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  	unique: true,
+  	required: true,
+  }
 }, {timestamps: true});
-
 
 var Admin = mongoose.model('Admin', AdminSchema)
 module.exports = Admin
